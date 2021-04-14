13cd3264e300f7601a3cd7e6152219e88ceec45a
minho-sama <n.a.minh1106@gmail.com>
2021-04-14 22:49:50 +0200

    lint

--- src/index.js
+++ src/index.js
@@ -1,8 +1,22 @@
-import { popUp, changeActive, projectHandling, appendProjects, showProjects, addDataToProjects, loadProjects} from "./dom";
-import {showDescription, discardTask, addTodo, loadProjectsFromPopUp, addDeleteEvent} from "./todoHandling"
+import { 
+    popUp, 
+    changeActive, 
+    projectHandling, 
+    appendProjects, 
+    showProjects, 
+    addDataToProjects, 
+    loadProjects
+} from "./dom";
 
-//uncomment if 'You Projects' is always on if there are no projects!
+import {
+    showDescription,
+    discardTask,
+    addTodo,
+    loadProjectsFromPopUp,
+    addDeleteEvent
+    } from "./todoHandling"
 
+//uncomment -->  'Your Projects' is ALWAYS on if there are no projects!
 if (localStorage.hasOwnProperty('projects') == false /*|| JSON.parse(localStorage.projects).length === 0*/){
     let defaultProjects = JSON.stringify(['Your Project'])
     localStorage.setItem('projects', defaultProjects)
